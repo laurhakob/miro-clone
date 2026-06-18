5d62f24dc2f285a4b31bff8645c59b3e4b434b21
laurhakob <laurhakob@gmail.com>
2026-06-19 00:34:39 +0400

    canvas layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,10 +12,11 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
       <Sidebar />
       <div className="pl-15 h-full">
         <div className="flex gap-x-3 h-full">
-            <OrgSidebar />
+          <OrgSidebar />
           <div className="h-full flex-1">
             <Navbar />
-            {children}</div>
+            {children}
+          </div>
         </div>
       </div>
     </main>
---
+++ app/board/[boardId]/page.tsx
@@ -0,0 +1,15 @@
+import { Canvas } from "./_components/canvas";
+
+interface BoardIdPageProps {
+  params: Promise<{
+    boardId: string;
+  }>;
+}
+
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { boardId } = await params;
+
+  return <Canvas boardId={boardId} />;
+};
+
+export default BoardIdPage;
